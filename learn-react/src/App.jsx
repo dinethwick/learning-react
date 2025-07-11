ca32c722a93dbbd4bd11ece590b0a213f64251f7
dinethwick <dinethwick23@gmail.com>
2025-07-11 23:53:43 +0930

    learning how to use states and hooks

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
     <div className="card">
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(true)}>
+        {hasLiked ? 'Liked': 'Like'}
+      </button>
     </div>
   )
 }
 
 const App = () => {
+  
+
   return(
     <div className="card-container">
       <h2>Functional arrow component</h2>
